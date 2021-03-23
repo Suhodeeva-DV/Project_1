173ae918cbab0e956dcf908ec18f0291729e531e
darya <darya.suhodeeva@yandex.ru>
2021-03-24 00:55:31 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Project_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите Ваше имя: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Привет, " + name + "!");
+            Console.Read();
+            Console.Clear();
+        }
+    }
+}
